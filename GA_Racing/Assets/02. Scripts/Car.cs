f9debb1cced8e118c7c3fa9b3766b2fa963b406b
BuildTools <unconfigured@null.spigotmc.org>
2018-04-17 15:40:54 +0900

    Main Files

---
+++ GA_Racing/Assets/02. Scripts/Car.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+    [Range(1,15)]
+    public int speed= 10;
+    public DataAsset mydata;
+    public Color carcolor;
+    private Material material;
+    private MeshRenderer meshRenderer;
+    public bool isStarted = false;
+    // Use this for initialization
+    //public void Awake()
+    //{
+    //    //material = this.transform.GetComponent<Material>();
+    //    meshRenderer = this.transform.GetComponent<MeshRenderer>();
+    //    material = meshRenderer.material;
+    //}
+    //public void Reset()
+    //{
+        
+    //    this.transform.GetComponent<MeshRenderer>().material.color = carcolor;
+    //    Debug.Log("Reset메소드 콜!");
+    //}
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isStarted)
+        {
+            this.transform.Translate(Vector3.forward * speed*Time.deltaTime);
+        }
+	}
+    public void RunStart()
+    {
+        isStarted = true;
+    }
+    IEnumerator RootUpdate()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
+}
---
+++ GA_Racing/Assets/SaveAsset/DataAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DataAsset : ScriptableObject {
+    public int[] value=new int[100];
+    //static void CreateDataAssetInstance()
+    //{
+    //    DataAsset dataAsset = CreateInstance<DataAsset>();
+    //}
+    //static void CreateDataAsset()
+    //{
+    //    DataAsset dataAsset = CreateInstance<DataAsset>();
+        
+        
+    //}
+}
